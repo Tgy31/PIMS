9b7dba2da9a852cc9ff6a7a1061832b179b24b0e
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-11-19 19:29:38 +0000

    copy to inspectors

--- WebContent/Ressources/js/inspectors.js
+++ WebContent/Ressources/js/inspectors.js
@@ -1,5 +1,17 @@
+var showRender = function ( data, type, full, meta ) {
+    return '<a href="./'+data+'/">Show</a>';
+};
+
 $(document).ready( function () {
     $('#inspector-table').DataTable( {
-        "ajax": '/PIMS/inspectors.json'
+        "ajax": '/PIMS/inspectors.json',
+        "columnDefs": [{
+            "targets": 3,
+            "render": showRender
+          }]
     });
+    
+    $('#inspector-table').on( 'click', 'tbody tr', function () {
+    	window.location.href = $(this).attr('href');
+    } );
 } );
