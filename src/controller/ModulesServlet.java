82fb4e371892b028b1739ffb3a62fe66771e74cb
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-11-20 23:19:26 +0000

    remove print

--- src/controller/ModulesServlet.java
+++ src/controller/ModulesServlet.java
@@ -32,7 +32,6 @@ public class ModulesServlet extends BootstrapServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String moduleSlug = this.getModuleSlug(request);
-		System.out.println(moduleSlug);
 		//Student student = this.getStudentBySlug(studentSlug);
 		
 		if (moduleSlug != null) {
