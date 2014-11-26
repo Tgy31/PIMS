b1a4d3b1792752ce0cf34886cae1c0483e9a47ba
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-11-26 13:52:17 +0000

    add knockout application

--- WebContent/Keywords.jsp
+++ WebContent/Keywords.jsp
@@ -7,11 +7,17 @@
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h3 class="panel-title">Keywords</h3>
+		<h3 class="panel-title">Keywords for ${ user.getFirst_name() }</h3>
 	</div>
 	<div class="panel-body">
-	    
+	
 	</div>
+	<ul class="list-group" data-bind="foreach: seats">
+	    <li class="list-group-item">
+	    	<span data-bind="text: name">
+	    	</span>
+	    </li>
+	</ul>
 </div>
 
 <%@ include file="Footer.jsp" %>
--- src/controller/KeywordsServlet.java
+++ src/controller/KeywordsServlet.java
@@ -1,11 +1,16 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.InspectorDAO;
+import model.dao.StudentDAO;
+import model.entity.User;
 
 /**
  * Servlet implementation class KeywordsServlet
@@ -20,12 +25,21 @@ public class KeywordsServlet extends BootstrapServlet {
     public KeywordsServlet() {
         super();
         this.layoutType = LayoutType.Grid;
+        
+        this.addJavascriptFile("knockout-3.2.0.js");
+        this.addJavascriptFile("keywords.js");
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userType = request.getParameter("type");
+		String userID = request.getParameter("id");
+		
+		User user = this.getUserWithTypeAndID(userType, userID);
+		request.setAttribute("user", user);
 
 		this.proceedGet("/Keywords.jsp", request, response);
 	}
@@ -35,6 +49,28 @@ public class KeywordsServlet extends BootstrapServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	}
+	
+	private User getUserWithTypeAndID(String userType, String userID) {
+		
+		User user = null;
+		int id = Integer.parseInt(userID);
+		
+		switch (userType) {
+			case "student": {
+				StudentDAO studentDAO = new StudentDAO();
+				user = studentDAO.findByStudentID(id);
+		        this.relatedMenuClass = "students";
+		        break;
+			}
+			case "inspector": {
+				InspectorDAO inspectorDAO = new InspectorDAO();
+				user = inspectorDAO.findByInspectorID(id);
+		        this.relatedMenuClass = "inspectors";
+		        break;
+			}
+		}
+		return user;
 	}
 
 }
