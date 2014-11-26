c14b66ea6172425b54a0c153f81a19f25f55beca
cxz413 <zcx0324@gmail.com>
2014-11-26 14:43:39 +0000

    KeywordMapping and Keyword

---
+++ src/model/dao/KeywordDAO.java
@@ -0,0 +1,10 @@
+package model.dao;
+
+import model.db.Template;
+import model.entity.Keyword;
+import model.mapping.KeywordMapping;
+
+public class KeywordDAO {
+	private Template template = new Template();
+	
+}
